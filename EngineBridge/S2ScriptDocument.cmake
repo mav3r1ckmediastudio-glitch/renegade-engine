a4e8ff655abed6859ae5145295c9e922d5c9ae40
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-03 21:26:35 +0100

    S2: register script document bridge sources

---
+++ EngineBridge/S2ScriptDocument.cmake
@@ -0,0 +1,5 @@
+target_sources(RenegadeEngineBridge PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/include/renegade/bridge/ScriptDocumentService.h
+    ${CMAKE_CURRENT_LIST_DIR}/src/ScriptDocumentService.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/src/ScriptDocumentEdits.cpp
+)
