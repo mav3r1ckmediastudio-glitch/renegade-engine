f3b40e7f7fe876508b4c96c796350054dfd4df0a
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-04 00:40:55 +0100

    S4A: register metadata service

---
+++ EngineBridge/S4AScriptMetadata.cmake
@@ -0,0 +1,4 @@
+target_sources(RenegadeEngineBridge PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/include/renegade/bridge/ScriptMetadataService.h
+    ${CMAKE_CURRENT_LIST_DIR}/src/ScriptMetadataService.cpp
+)
