4bff832d211e323fcc631f6995f4eea9c078240f
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-04 15:57:00 +0100

    S5A: register governed Lua gameplay acceptance

--- Tests/S5CoreGameplayApi.cmake
+++ Tests/S5CoreGameplayApi.cmake
@@ -30,6 +30,38 @@ add_test(
     COMMAND RenegadeS5CoreGameplayApiTests
 )
 
+add_executable(RenegadeS5CoreGameplayLuaTests
+    ${CMAKE_CURRENT_LIST_DIR}/S5CoreGameplayLuaTests.cpp
+)
+
+target_link_libraries(
+    RenegadeS5CoreGameplayLuaTests
+    PRIVATE
+        Renegade::RuntimeBootstrap
+)
+
+target_compile_options(
+    RenegadeS5CoreGameplayLuaTests
+    PRIVATE
+        "$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
+)
+
+set_target_properties(
+    RenegadeS5CoreGameplayLuaTests
+    PROPERTIES
+        FOLDER "Renegade/Tests"
+)
+
+add_dependencies(
+    RenegadeBridgeTests
+    RenegadeS5CoreGameplayLuaTests
+)
+
+add_test(
+    NAME RenegadeS5CoreGameplayLuaTests
+    COMMAND RenegadeS5CoreGameplayLuaTests
+)
+
 add_test(
     NAME RenegadeS5CoreGameplayApiSourceContract
     COMMAND ${CMAKE_COMMAND}
