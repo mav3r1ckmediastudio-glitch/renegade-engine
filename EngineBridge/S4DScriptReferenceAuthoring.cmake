b9d77d2055741477af8fc891a5fba348c4fe97e1
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-04 14:03:48 +0100

    S4D: register script reference authoring service

---
+++ EngineBridge/S4DScriptReferenceAuthoring.cmake
@@ -0,0 +1,6 @@
+target_sources(
+    RenegadeEngineBridge
+    PRIVATE
+        ${CMAKE_CURRENT_LIST_DIR}/src/ScriptReferenceAuthoringService.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/include/renegade/bridge/ScriptReferenceAuthoringService.h
+)
