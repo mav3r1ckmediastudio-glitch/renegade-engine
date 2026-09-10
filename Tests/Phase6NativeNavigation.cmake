23d4d9893c4a3960bec5d2db9521131b7614329e
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-10 10:25:14 +0100

    Navigation: register native pathfinding test target

---
+++ Tests/Phase6NativeNavigation.cmake
@@ -0,0 +1,37 @@
+add_executable(RenegadePhase6NativeNavigationTests
+    ${CMAKE_CURRENT_LIST_DIR}/Phase6NativeNavigationTests.cpp
+)
+
+target_link_libraries(
+    RenegadePhase6NativeNavigationTests
+    PRIVATE
+        Renegade::EngineBridge
+)
+
+target_compile_options(
+    RenegadePhase6NativeNavigationTests
+    PRIVATE
+        "$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
+)
+
+set_target_properties(
+    RenegadePhase6NativeNavigationTests
+    PROPERTIES
+        FOLDER "Renegade/Tests"
+)
+
+add_dependencies(
+    RenegadeBridgeTests
+    RenegadePhase6NativeNavigationTests
+)
+
+add_test(
+    NAME RenegadePhase6NativeNavigationTests
+    COMMAND RenegadePhase6NativeNavigationTests
+)
+
+set_tests_properties(
+    RenegadePhase6NativeNavigationTests
+    PROPERTIES
+        LABELS "Phase6;Navigation"
+)
