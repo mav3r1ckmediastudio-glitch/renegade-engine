326133f7e32cc6e11f05ffd8069a9f09462181a3
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-11 12:32:37 +0100

    Phase 7: register consolidated A-E Studio sources

---
+++ Studio/Phase7Native.cmake
@@ -0,0 +1,6 @@
+target_sources(RenegadeStudio
+    PRIVATE
+        "${CMAKE_CURRENT_LIST_DIR}/src/Phase7NativeInspector.h"
+        "${CMAKE_CURRENT_LIST_DIR}/src/Phase7AnimationInspector.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/src/Phase7SpecialistInspector.cpp"
+)
