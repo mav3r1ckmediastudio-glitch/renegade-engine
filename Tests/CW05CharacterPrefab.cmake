bcff3ea2b3f29b3aaca6614f27daa071056ea2ae
mav3r1ckmediastudio-glitch <mav3r1ckmediastudio@gmail.com>
2026-09-15 00:09:01 +0100

    CW-05: register Character prefab tests

---
+++ Tests/CW05CharacterPrefab.cmake
@@ -0,0 +1,37 @@
+add_executable(RenegadeCW05CharacterPrefabTests
+    CW05CharacterPrefabTests.cpp
+)
+
+target_link_libraries(
+    RenegadeCW05CharacterPrefabTests
+    PRIVATE
+        Renegade::EngineBridge
+)
+
+target_compile_options(
+    RenegadeCW05CharacterPrefabTests
+    PRIVATE
+        "$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
+)
+
+set_target_properties(
+    RenegadeCW05CharacterPrefabTests
+    PROPERTIES FOLDER "Renegade/Tests"
+)
+
+add_dependencies(
+    RenegadeBridgeTests
+    RenegadeCW05CharacterPrefabTests
+)
+
+add_test(
+    NAME RenegadeCW05CharacterPrefabTests
+    COMMAND RenegadeCW05CharacterPrefabTests
+)
+
+add_test(
+    NAME RenegadeCW05CharacterPrefabSourceContract
+    COMMAND ${CMAKE_COMMAND}
+        -DRENEGADE_SOURCE_DIR=${CMAKE_SOURCE_DIR}
+        -P ${CMAKE_SOURCE_DIR}/Tests/CW05CharacterPrefabSourceContract.cmake
+)
